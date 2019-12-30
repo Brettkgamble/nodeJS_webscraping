8d0787ae5e954b4b8fac6282179d238737cce5d0
Brett Gamble <brettkgamble@gmail.com>
2019-12-30 14:42:01 -0700

    0002

--- Sandbox/scrapingintro/index.js
+++ Sandbox/scrapingintro/index.js
@@ -3,15 +3,15 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 
 async function main() {
-    const html = await request.get("https://reactnativetutorial.net/css-selectors/");
+    const html = await request.get("https://reactnativetutorial.net/css-selectors/lesson2.html");
     // const html = await request.get("https://www.lakewoodchev.com/used-inventory/index.htm?start");
 
     fs.writeFileSync("./test.html", html);
 
     const $ = cheerio.load(html);
-    const theText = $("h1").text();
-    console.log(theText);
-
+    $("h2").each((index, element) => {
+        console.log($(element).text());
+    });
 }
 
 main();
